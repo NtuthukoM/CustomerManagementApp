e8ba4f59402266408c0afbde5d8124593b0bc06f
Ntuthuko <ntuthukorocks@gmail.com>
2023-03-24 07:24:00 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+using CustomerManagementApp.Core.Data;
+using CustomerManagementApp.Core.Implementation;
+using CustomerManagementApp.Core.Interfaces;
+
+var customers = CustomerData.GetCustomers();
+
+ICustomerReport customerReport = new CustomerReport();
+
+string reportData = customerReport.GenerateSummaryReport(customers);
+Console.WriteLine(reportData);
+Console.Read();
